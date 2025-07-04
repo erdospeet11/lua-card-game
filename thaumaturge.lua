6ccc90ef42059225609cc7500e91f5c94a442975
Peter Erdos <erdospepe9@gmail.com>
2025-07-04 18:32:14 +0200

    Fixed scale for player card

--- thaumaturge.lua
+++ thaumaturge.lua
@@ -11,10 +11,19 @@ end
 
 function Thaumaturge:draw()
     if CardBack then
-        local sx = self.width / CardBack:getWidth()
-        local sy = self.height / CardBack:getHeight()
+        -- Maintain aspect ratio by using the smaller scale factor
+        local scale_x = self.width / CardBack:getWidth()
+        local scale_y = self.height / CardBack:getHeight()
+        local scale = math.min(scale_x, scale_y)
+        
+        -- Calculate centered position
+        local scaled_width = CardBack:getWidth() * scale
+        local scaled_height = CardBack:getHeight() * scale
+        local draw_x = self.x - scaled_width/2
+        local draw_y = self.y - scaled_height/2
+        
         lg.setColor(1,1,1,1)
-        lg.draw(CardBack, self.x - self.width/2, self.y - self.height/2, 0, sx, sy)
+        lg.draw(CardBack, draw_x, draw_y, 0, scale, scale)
     else
         Player.draw(self)
     end
